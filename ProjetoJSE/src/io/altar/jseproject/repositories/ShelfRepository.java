76696d9bc21c2e1df286a615aa20dc3484d6ec92
eduardorito15 <eduardorito15@gmail.com>
2025-07-22 17:12:59 +0100

    update

--- ProjetoJSE/src/io/altar/jseproject/repositories/ShelfRepository.java
+++ ProjetoJSE/src/io/altar/jseproject/repositories/ShelfRepository.java
@@ -1,8 +1,9 @@
 package io.altar.jseproject.repositories;
 
 import io.altar.jseproject.model.Shelf;
+import io.altar.jseproject.repositories.interfaces.ShelfInterface;
 
-public class ShelfRepository extends EntityRepository<Shelf> {
+public class ShelfRepository extends EntityRepository<Shelf> implements ShelfInterface {
 	private static final ShelfRepository INSTANCE = new ShelfRepository();
 
 	private ShelfRepository() {
